1c12190bdd7790a665eb3801cf62354322c8b25b
Alex Oberhofer <alexmoberhofer@gmail.com>
2021-04-30 18:23:16 -0400

    Add more content to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import logo from "./logo.svg";
-import "./App.css";
 import "./css/pure-min.css";
 import "./css/normalize.css";
 import "./css/grids-responsive-min.css";
+import "./css/icono.min.css";
+import "./css/main.css";
 
 function App() {
   return (
@@ -69,9 +69,100 @@ function App() {
         </div>
 
         <div className="content">
-            <h2 className="content-subhead">Welcome!</h2>
+          <h2 className="content-subhead">Welcome!</h2>
         </div>
 
+        <p>
+          Welcome to my personal web page. This page hosts information on my
+          software development projects and interests. Please check out my
+          GitHub page for my latest activity or my Linkedin for professional
+          inquiries.
+        </p>
+
+        <div className="home-well-heading">
+          <p>Click an icon below to be taken my profile:</p>
+        </div>
+
+        <div className="pure-g home-well">
+          <div className="pure-u-1-2 home-half">
+            <a
+              className="fa fa-github-square fa-5x"
+              aria-hidden="true"
+              title="Github"
+              target="_blank"
+              href="https://github.com/AlexOberhofer"
+            ></a>
+          </div>
+
+          <div className="pure-u-1-2 home-half">
+            <a
+              className="fa fa-linkedin-square fa-5x"
+              aria-hidden="true"
+              title="LinkedIn"
+              target="_blank"
+              href="https://linkedin.com/in/alex-oberhofer-5403bb143/"
+            ></a>
+          </div>
+
+          <div className="home-well-heading">
+            <p>
+              Click a button below to be taken to one of my code repositories:
+            </p>
+          </div>
+
+          <div className="pure-g home-well">
+            <div className="pure-u-1 pure-u-md-1-5">
+              <a
+                className="pure-button repo-button repo-button"
+                target="_blank"
+                href="https://github.com/AlexOberhofer/SDL2-GNUBoy"
+              >
+                SDL2 GNUBoy
+              </a>
+            </div>
+            <br />
+            <div className="pure-u-1 pure-u-md-1-5">
+              <a
+                className="pure-button repo-button"
+                target="_blank"
+                href="https://github.com/AlexOberhofer/Space-Invaders"
+              >
+                Space Invaders
+              </a>
+            </div>
+            <br />
+            <div className="pure-u-1 pure-u-md-1-5">
+              <a
+                className="pure-button repo-button"
+                target="_blank"
+                href="https://github.com/AlexOberhofer/Chip-8-Emulator"
+              >
+                Chip 8
+              </a>
+            </div>
+            <br />
+            <div className="pure-u-1 pure-u-md-1-5">
+              <a
+                className="pure-button repo-button"
+                target="_blank"
+                href="https://github.com/AlexOberhofer/sdl2-doom"
+              >
+                SDL2 DOOM
+              </a>
+            </div>
+            <br />
+            <div className="pure-u-1 pure-u-md-1-5">
+              <a
+                className="pure-button repo-button"
+                target="_blank"
+                href="https://github.com/AlexOberhofer/pylife"
+              >
+                Pylife
+              </a>
+            </div>
+            <br />
+          </div>
+        </div>
         <script>doMenuMagic(this, this.document);</script>
       </div>
     </div>
