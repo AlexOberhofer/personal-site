1bdee2ed25b2a4bd0ef0892a4c9c0d91895c0775
Alex Oberhofer <alexmoberhofer@gmail.com>
2021-04-30 18:51:50 -0400

    add navbar as a component

--- src/App.tsx
+++ src/App.tsx
@@ -7,70 +7,26 @@ import "./css/main.css";
 function App() {
   return (
     <div className="App">
+      <link
+        rel="stylesheet"
+        href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+      />
+      <link
+        rel="stylesheet"
+        href="https://unpkg.com/purecss@2.0.3/build/pure-min.css"
+        integrity="sha384-cg6SkqEOCV1NbJoCu11+bm0NvBRc8IYLRGXkmNrqUBfTjmMYwNKPWBTIKyw9mHNJ"
+      ></link>
       <script src="../public/javascript/scripts.js"></script>
 
-      <div className="header">
-        <div className="custom-menu-wrapped">
-          <div className="pure-menu custom-menu custom-menu-top">
-            <a
-              href="javascript:history.back()"
-              className="pure-menu-link back-arrow"
-            >
-              &#9668;
-            </a>
-            <a href="#" className="custom-menu-toggle" id="toggle">
-              <s className="bar"></s>
-              <s className="bar"></s>
-            </a>
-          </div>
-          <div
-            className="home-menu pure-menu pure-menu-horizontal pure-menu-fixed pure-menu-scrollable custom-menu custom-menu-bottom custom-menu-tucked"
-            id="tuckedMenu"
-          >
-            <ul className="pure-menu-list">
-              <li className="pure-menu-item">
-                <a className="pure-menu-link" href="/">
-                  Home
-                </a>
-              </li>
-              <li className="pure-menu-item">
-                <a
-                  className="pure-menu-link"
-                  target="_blank"
-                  href="https://github.com/AlexOberhofer"
-                >
-                  GitHub
-                </a>
-              </li>
-              <li className="pure-menu-item">
-                <a className="pure-menu-link" href="public/html/gnuboy.html">
-                  GNUBoy
-                </a>
-              </li>
-              <li className="pure-menu-item">
-                <a className="pure-menu-link" href="public/html/projects.html">
-                  Other Projects
-                </a>
-              </li>
-              <li className="pure-menu-item">
-                <a className="pure-menu-link" href="public/html/about.html">
-                  About
-                </a>
-              </li>
-            </ul>
-          </div>
+      <div className="banner">
+        <div id="banner-img" className="banner-head">
+          <p>Alex Oberhofer</p>
+          <p>Charlotte, NC Software Developer</p>
         </div>
+      </div>
 
-        <div className="banner">
-          <div id="banner-img" className="banner-head">
-            <p>Alex Oberhofer</p>
-            <p>Charlotte, NC Software Developer</p>
-          </div>
-        </div>
-
-        <div className="content">
-          <h2 className="content-subhead">Welcome!</h2>
-        </div>
+      <div className="content">
+        <h2 className="content-subhead">Welcome!</h2>
 
         <p>
           Welcome to my personal web page. This page hosts information on my
@@ -163,7 +119,6 @@ function App() {
             <br />
           </div>
         </div>
-        <script>doMenuMagic(this, this.document);</script>
       </div>
     </div>
   );
---
+++ src/component/Navbar.tsx
@@ -0,0 +1,65 @@
+import "../css/pure-min.css";
+import "../css/normalize.css";
+import "../css/grids-responsive-min.css";
+import "../css/icono.min.css";
+import "../css/main.css";
+
+export function Navbar() {
+  return (
+    <>
+      <div className="custom-menu-wrapped">
+        <div className="pure-menu custom-menu custom-menu-top">
+          <a
+            href="javascript:history.back()"
+            className="pure-menu-link back-arrow"
+          >
+            &#9668;
+          </a>
+          <a href="#" className="custom-menu-toggle" id="toggle">
+            <s className="bar"></s>
+            <s className="bar"></s>
+          </a>
+        </div>
+        <div
+          className="home-menu pure-menu pure-menu-horizontal pure-menu-fixed pure-menu-scrollable custom-menu custom-menu-bottom custom-menu-tucked"
+          id="tuckedMenu"
+        >
+          <ul className="pure-menu-list">
+            <li className="pure-menu-item">
+              <a className="pure-menu-link" href="/">
+                Home
+              </a>
+            </li>
+            <li className="pure-menu-item">
+              <a
+                className="pure-menu-link"
+                target="_blank"
+                href="https://github.com/AlexOberhofer"
+              >
+                GitHub
+              </a>
+            </li>
+            <li className="pure-menu-item">
+              <a className="pure-menu-link" href="public/html/gnuboy.html">
+                GNUBoy
+              </a>
+            </li>
+            <li className="pure-menu-item">
+              <a className="pure-menu-link" href="public/html/projects.html">
+                Other Projects
+              </a>
+            </li>
+            <li className="pure-menu-item">
+              <a className="pure-menu-link" href="public/html/about.html">
+                About
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <script>doMenuMagic(this, this.document);</script>
+    </>
+  );
+}
+
+export default Navbar;
