4f3870edb03a132147cd4925892cd6df4c0fa294
Alex Oberhofer <alexmoberhofer@gmail.com>
2021-04-30 21:16:19 -0400

    add react-helmet

--- src/component/HomepageContent.tsx
+++ src/component/HomepageContent.tsx
@@ -6,12 +6,6 @@ import { ReactElement } from "react";
 export const HomePageContent = (): ReactElement => {
   return (
     <>
-      <head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1"
-        ></meta>
-      </head>
       <Navbar />
       <body>
         <Home />
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,23 @@ import ReactDOM from "react-dom";
 import HomepageContent from "./component/HomepageContent";
 import { ReactElement } from "react";
 import "./index.css";
+import { Helmet } from "react-helmet";
 
 const App = (): ReactElement => {
-    return (
+  return (
+    <div>
+      <Helmet>
+        <meta
+          charSet="utf-8"
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
+        <title>Alex Oberhofer</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
       <HomepageContent />
-    )
-}
+    </div>
+  );
+};
 
-ReactDOM.render(<App />,document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
