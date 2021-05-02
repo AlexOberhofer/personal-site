64c3d0012a1b134178d3839dec99617fbaf18e64
Alex Oberhofer <alexmoberhofer@gmail.com>
2021-05-02 15:23:16 -0400

    update footer content

--- src/component/AboutPageContent.tsx
+++ src/component/AboutPageContent.tsx
@@ -10,7 +10,6 @@ export const AboutPageContent = (): ReactElement => {
       <body>
         <About />
       </body>
-      <Footer />
     </>
   );
 };
--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -2,6 +2,9 @@ import "../css/pure-min.css";
 import "../css/normalize.css";
 import "../css/grids-responsive-min.css";
 import "../css/main.css";
+import linuxBadge from "../images/badges/linux.png";
+import firefoxBadge from "../images/badges/get_firefox.png";
+import vimBadge from "../images/badges/vim_the_editor.png";
 import { ReactElement } from "react";
 
 export const Footer = (): ReactElement => {
@@ -25,16 +28,15 @@ export const Footer = (): ReactElement => {
 
             <div className="pure-u-1 pure-u-md-1-3">
                 <div className="footer-center">
-                    &nbsp;&nbsp;<a href="https://www.kernel.org/"><img alt="Linux" src="https://raw.githubusercontent.com/AlexOberhofer/web-images/master/images/badges/linux.png" /></a>
-                    &nbsp;&nbsp;<a href="https://www.vim.org/"><img alt="Vim" src="https://raw.githubusercontent.com/AlexOberhofer/web-images/master/images/badges/vim_the_editor.png" /></a>
-                    &nbsp;&nbsp;<a href="https://www.mozilla.org/en-US/firefox/developer/"><img alt="Firefox" src="https://raw.githubusercontent.com/AlexOberhofer/web-images/master/images/badges/get_firefox.png" /></a>
+                    &nbsp;&nbsp;<a href="https://www.kernel.org/"><img alt="Linux" src={linuxBadge} /></a>
+                    &nbsp;&nbsp;<a href="https://www.vim.org/"><img alt="Vim" src={vimBadge} /></a>
+                    &nbsp;&nbsp;<a href="https://www.mozilla.org/en-US/firefox/developer/"><img alt="Firefox" src={firefoxBadge} /></a>
                 </div>
 
             </div> <br/><br/><br/>
 
             <div className="pure-u-1 pure-u-md-1-3">
                 <div className="footer-right">
-                    Built with <a target="_blank" rel="noreferrer" href="https://purecss.io/">Pure.css</a> v2.0.3<br/>
                     &copy; 2016-{getYear()} Alex Oberhofer<br/>
                 </div>
             </div>
