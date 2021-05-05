ad802e2d4dae58f7e912ca49245661578f40ded6
Alex Oberhofer <alexmoberhofer@gmail.com>
2021-05-04 21:35:00 -0400

    updates to about content

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -42,7 +42,6 @@ export const About = (): ReactElement => {
                 <li>
                   <i
                     className="fa fa-github-square fa-2x"
-                    aria-hidden="true"
                   ></i>
                   <a
                     href="https://github.com/AlexOberhofer"
@@ -71,11 +70,22 @@ export const About = (): ReactElement => {
       </div>
       <div>
         <div className="col-sm-4 col-sm-offset-1">
-          <h1>Projects:</h1>
+
+          <h1>Current:</h1>
+          <br />
+          <p>
+            I currently live in Charlotte, North Carolina, where I work as a
+            software engineer. My current research interests include hardware
+            emulation, Arduino, single board computers, operating systems, and
+            GNU/Linux Systems.
+          </p>
+          <br />
+
+          <h1>Development Interests:</h1>
           <br />
           <p>
             I have numerous side projects I enjoy hacking on in my free time. I
-            generally devote my time to devloping or porting retro gaming
+            generally devote my time to developing or porting retro gaming
             software and emulators. You can check out some of my projects at my
             github page on the sidebar.
             <br />
@@ -85,21 +95,11 @@ export const About = (): ReactElement => {
             license when using the code. Enjoy!
           </p>
 
-          <h1>Current Work:</h1>
+          <h1>Early Development Experience:</h1>
           <br />
           <p>
-            I currently live in Charlotte, North Carolina, where I work as a
-            software developer. My current research interests include hardware
-            emulation, Arduino, single board computers, operating systems, and
-            GNU/Linux Systems.
-          </p>
-          <br />
-
-          <h1>Previous Work:</h1>
-          <br />
-          <p>
-            During my Senior year I worked as Research Technician for the
-            Program for the Study of Developed Shorelines at WCU. As a research
+            During my Senior year at WCU I worked as Research Technician for the
+            Program for the Study of Developed Shorelines. As a research
             technician, I worked to develop and maintaining the Beach
             Nourishment Viewer web application in node.js. More information
             about the{" "}
@@ -116,20 +116,26 @@ export const About = (): ReactElement => {
             with other students to create applications for ARM devices with the
             intent to use these programs to test development chips. These
             experiences solidified my interests in computer science, and after
-            graduating from high school in 2013 I moved to Western Carolina
+            graduating from high school in 2013 I attended Western Carolina
             University to study computer science and mathematics.
           </p>
         </div>
 
         <div className="col-sm-4 col-sm-offset-1">
-          <h1>Work</h1>
+          <h1>Employment History</h1>
           <ul>
             <li>
-              SCI Fusion360 | Associate Software Developer | 2017 - 2021{" "}
-              <br />{" "}
+              MIB | Software Engineer | 2021 - Present
+              <br />
             </li>
-          </ul>
-          <ul>
+            <li>
+              SCI Fusion360 | Staff Software Developer | 2021 - 2021
+              <br />
+            </li>
+            <li>
+              SCI Fusion360 | Associate Software Developer | 2017 - 2021
+              <br />
+            </li>
             <li>
               Program for the Study of Developed Shorelines | Web Developer |
               2016 - 2017 <br />
@@ -182,11 +188,6 @@ export const About = (): ReactElement => {
             <li>
               <a href="https://github.com/AlexOberhofer/Chip-8-Emulator">
                 Chip 8 Interpeter/Emulator
-              </a>
-            </li>
-            <li>
-              <a href="http://beachnourishment.wcu.edu">
-                PSDS Beach Nourishment Viewer
               </a>
             </li>
             <li>
